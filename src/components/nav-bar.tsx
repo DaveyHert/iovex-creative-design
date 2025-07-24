5a0425288484bd80f12ff8be90ca50ede288f458
daveyhert <daveyhert@gmail.com>
2025-07-24 22:33:10 +0100

    implement floating particle animation hook with particle system management

--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -1,10 +1,12 @@
+import { Logo } from "../assets/icons";
+
 function NavBar() {
   return (
     <nav className='fixed max-w-[893px] w-[90%] left-1/2 -translate-x-1/2 top-[25px] z-50 flex justify-between items-center rounded-b-full backdrop-blur-lg  bg-[linear-gradient(rgba(117,117,117,0.4)_0%,rgba(51,51,51,0.45)_100%)]'>
       <div className='logo'>
-        <img src='./img/icons/logo2.svg' alt='logo' />
+        <Logo />
       </div>
-      <ul className='nav-links'>
+      <ul className='nav-links lisat'>
         <li className='nav-link'>
           <a href='#'>
             <img src='./img/icons/quicksilver.png' alt='link' />
